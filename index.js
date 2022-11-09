68517bccc13121b17cc4d3041ea6aa4a8191ea72
Daelni <ddaelni@gmail.com>
2022-11-09 01:35:23 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function ocultarOptionCreate(){
+    console.log('me ejecuto')
+    // document.getElementsByClassName('ocultar')[0].style.display="none" Así se llama una clase en js
+    document.getElementById("ocultar").style.display="none";
+}

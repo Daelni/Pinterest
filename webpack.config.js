e02e8049e3368c768b6c3a2e3ba3790cc40b49de
JDemn <dsoftwaremancilla@outlook.es>
2023-01-29 00:20:47 -0600

    adding npm manage packages, webpack for production building and babel to make this project compatible with other old browers

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ]
+};
